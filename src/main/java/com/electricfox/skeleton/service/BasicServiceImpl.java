3bc2e4cc2d3013e053c9768bc4abc4dc8f6036e2
Alex Bowers <alex.bowers@amido.com>
2020-02-16 09:18:21 +0000

    wire sevice layer into repo layer

--- src/main/java/com/electricfox/skeleton/service/BasicServiceImpl.java
+++ src/main/java/com/electricfox/skeleton/service/BasicServiceImpl.java
@@ -1,16 +1,26 @@
 package com.electricfox.skeleton.service;
 
+import com.electricfox.skeleton.repository.BasicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BasicServiceImpl implements BasicService{
+
+    BasicRepository basicRepository;
+
+    @Autowired
+    public BasicServiceImpl(BasicRepository basicRepository) {
+        this.basicRepository = basicRepository;
+    }
+
     @Override
     public String basicServiceFunction() {
-        return null;
+        return basicRepository.basicRepoFunction();
     }
 
     @Override
     public String anotherBasicServiceFunction() {
-        return null;
+        return basicRepository.anotherBasicRepoFunction();
     }
 }
