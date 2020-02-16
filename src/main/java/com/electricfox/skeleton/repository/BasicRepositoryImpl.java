900ec9397c462f749198ed58741625efd4a634ad
Alex Bowers <alex.bowers@amido.com>
2020-02-16 09:19:00 +0000

    Add static results to repo layer and write basic tests

--- src/main/java/com/electricfox/skeleton/repository/BasicRepositoryImpl.java
+++ src/main/java/com/electricfox/skeleton/repository/BasicRepositoryImpl.java
@@ -5,13 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class BasicRepositoryImpl implements BasicRepository {
 
+    private static final String ANOTHER_BASIC_REPO_FUNCTION = "This is another basic repo function";
+    private static final String BASIC_REPO_FUNCTION = "This is a basic repo function";
+
     @Override
     public String basicRepoFunction() {
-        return null;
+        return BASIC_REPO_FUNCTION;
     }
 
     @Override
     public String anotherBasicRepoFunction() {
-        return null;
+        return ANOTHER_BASIC_REPO_FUNCTION;
     }
 }
--- src/test/java/com/electricfox/skeleton/repository/BasicRepositoryImplTest.java
+++ src/test/java/com/electricfox/skeleton/repository/BasicRepositoryImplTest.java
@@ -6,11 +6,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BasicRepositoryImplTest {
 
+    private static final String EXPECTED_ANOTHER_BASIC_FUNCTION = "This is another basic repo function";
+    private static final String EXPECTED_BASIC_FUNCTION = "This is a basic repo function";
+
     @Test
     void basicRepoFunction() {
+
+        //Arrange
+        BasicRepository classUnderTest = new BasicRepositoryImpl();
+
+        //Act
+        String result = classUnderTest.basicRepoFunction();
+
+        //Assert
+        assertEquals(EXPECTED_BASIC_FUNCTION, result,"The basic function was not as expected");
     }
 
     @Test
     void anotherBasicRepoFunction() {
+        //Arrange
+        BasicRepository classUnderTest = new BasicRepositoryImpl();
+
+        //Act
+        String result = classUnderTest.anotherBasicRepoFunction();
+
+        //Assert
+        assertEquals(EXPECTED_ANOTHER_BASIC_FUNCTION, result,"The other basic function was not as expected");
     }
 }
