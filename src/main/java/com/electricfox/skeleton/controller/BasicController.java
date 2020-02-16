1dd4fa4ad12ebba0fa294380d504a1e95b83c1d4
Alex Bowers <alex.bowers@amido.com>
2020-02-16 08:58:38 +0000

    add controller and empty test

--- src/main/java/com/electricfox/skeleton/controller/BasicController.java
+++ src/main/java/com/electricfox/skeleton/controller/BasicController.java
@@ -1,4 +1,24 @@
 package com.electricfox.skeleton.controller;
 
+import com.electricfox.skeleton.repository.BasicRepository;
+import com.electricfox.skeleton.service.BasicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BasicController {
+
+    BasicService basicService;
+
+    @Autowired
+    public BasicController(BasicService basicService) {
+        this.basicService = basicService;
+    }
+
+    @GetMapping("/basic-greeting")
+    public String getBasicGreeting(){
+        return basicService.basicServiceFunction();
+    }
+
 }
---
+++ src/test/java/com/electricfox/skeleton/controller/BasicControllerTest.java
@@ -0,0 +1,12 @@
+package com.electricfox.skeleton.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicControllerTest {
+
+    @Test
+    void getBasicGreeting() {
+    }
+}
